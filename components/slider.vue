fa24d53f2ed39e83769a6eca9a761bb4f31499ff
TutorFx <gabriel_serejo@live.com>
2021-12-29 03:27:22 -0300

    Update slider.vue

--- components/slider.vue
+++ components/slider.vue
@@ -1,10 +1,11 @@
 
 <template>
-  <v-carousel height="auto" v-model="model">
+  <v-carousel height="auto" v-model="model" :cycle="true">
     <v-carousel-item
       v-for="(color, i) in colors"
       :key="color"
       class="mb-0 pb-0"
+      eager
     >
       <v-sheet
         :color="color"
